f3915aeacb3277a9700bceece202e4bf410ad5e3
Tayyab <heyitstay011@gmail.com>
2021-12-10 21:21:51 +0530

    Channel Routes Debugging

--- routes/channel.js
+++ routes/channel.js
@@ -2,14 +2,15 @@ const router = require('express').Router();
 const { requireAuth } = require('../middlewares/authMiddlewares');
 const Channel = require('../models/channel');
 
+
 // get Channel data
-router.get('/:id', requireAuth, async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         let channel = await Channel.findById(req.params.id);
         if(!channel){
             return res.status(401).json({ error: 'This channel does not exists'});
         }
-        res.json(channel);
+        res.json({ channel });
     } catch (error) {
         res.status(500).json({error});
     }
@@ -23,9 +24,11 @@ router.post('/', requireAuth, async (req, res) => {
   
     try {
         let channel = await Channel.findOne({
-            $and: [{participants: { $in: [id1] }, participants: { $in: [id2] }}]  
+            $and: [
+                { participants: { $in: [id1] }}, 
+                { participants: { $in: [id2] }},
+            ]  
           });
-    
         if(channel){
             return res.status(401).json({ error: 'User Already Added'});
         }   
@@ -42,8 +45,8 @@ router.get('/user/:userId', requireAuth, async (req, res) => {
     const id = req.params.userId;
     try {
         let channels =  await Channel.find({
-            participant: { $in: [id] }
-        })
+            participants: { $in: [id] }
+        });
         res.json({ channels});
     } catch (error) {
         res.status(500).json({error});
