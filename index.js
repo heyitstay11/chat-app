294455009bc1d0509b0cc513116d1116261e0927
Tayyab <heyitstay011@gmail.com>
2021-12-11 17:21:55 +0530

    CORS - 1

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(express.json());
 app.use(express.urlencoded({'extended': false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
